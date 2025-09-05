a94e3fbdb8e99b53948c4e0a3786438dc1b45202
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2025-09-05 09:19:16 -0500

    se agrego el cv actualizado

--- src/Tecnologias.jsx
+++ src/Tecnologias.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+
 import './css/Tecnologias.css'; // Asegúrate de crear y vincular este archivo CSS
-import Curriculum from '/Cv_VELASQUEZ_ANCASI_GIANCARLOS_RENZO.pdf';
+import Curriculum from '/Cv_VELASQUEZ_ANCASI_GIANCARLOS_RENZO_2025.pdf';
 export const Tecnologias = () => {
   return (
     <section className="sectionAbout" id="About">
